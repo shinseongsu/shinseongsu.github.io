9d8b8391c7de7c16c28da0d3ccba6f3be83bd7f9
Shin seongsu <tlstjdtn321@naver.com>
2020-08-03 02:49:24 +0900

    custom.js로 변경

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -4,7 +4,7 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-/*(function($) {
+(function($) {
 
 	skel.breakpoints({
 		wide: '(min-width: 961px) and (max-width: 1880px)',
@@ -118,45 +118,4 @@
 
 	});
 
-})(jQuery); */
-
-$(document).ready(function(){
-	function imagesProgress(){
-		var $container = $("#progress"),
-			$progressBar = $container.find(".progress-bar"),
-			$progressText = $container.find(".progress-text"),
-			imgLoad = imagesLoaded("body"),	
-			imgTotal = imgLoad.images.length,	
-			imgLoaded = 0,										
-			current = 0,							
-			progressTimer = setInterval(updateProgress, 1000 / 60);
-		
-		imgLoad.on("progress", function(){
-			imgLoaded++;
-		});
-	
-		function updateProgress(){
-			var target = ( imgLoaded / imgTotal) * 100;
-			
-			current += ( target - current) * 0.1;
-			$progressText.text( Math.floor(current) + '%' );
-			
-			if(current >= 100){
-				clearInterval(progressTimer);
-				$container.addClass("progress-complete");
-				$progressBar.add($progressText)
-					.delay(500)
-					.animate({opacity: 0},250,function(){
-						$container.animate({top: '-100%'},1000,'easeInOutQuint');
-					});
-				$("body").addClass("active");
-			}
-			if(current > 99.9){
-				current = 100;
-			}
-			
-		}	
-	}
-
-	imagesProgress();
-});
+})(jQuery);
